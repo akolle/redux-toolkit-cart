e0dc633e3198eec3a83ef1304ca24085bc224dd2
akolle <andykoller55@yahoo.com>
2024-09-05 17:06:48 -0400

    hero icons

--- src/icons.js
+++ src/icons.js
@@ -1,48 +1,66 @@
 export const CartIcon = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6 '
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 "
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
       />
     </svg>
-  );
-};
+  )
+}
 
 export const ChevronDown = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
     </svg>
-  );
-};
+  )
+}
 
 export const ChevronUp = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
     </svg>
-  );
-};
+  )
+}
+
+/* KEEPING FOR REFERENCE ***
+// export const Testing = () => (
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     fill="none"
+//     viewBox="0 0 24 24"
+//     strokeWidth={1.5}
+//     stroke="currentColor"
+//     className="size-6"
+//   >
+//     <path
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+//     />
+//   </svg>
+// )
